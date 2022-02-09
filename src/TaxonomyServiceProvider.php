c5f02ad0346981b2c5b1733b3c4208b86a711fbf
Israel Ortuño <ai.ortuno@gmail.com>
2022-02-09 13:33:48 +0100

    Fix typo in database folder name

--- src/TaxonomyServiceProvider.php
+++ src/TaxonomyServiceProvider.php
@@ -10,7 +10,7 @@ class TaxonomyServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->mergeConfigFrom(__DIR__ . '/../config/taxonomy.php', 'taxonomy');
-        $this->loadMigrationsFrom(__DIR__ . '/../databsae/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
 
         // Pluck model keys based on model's configured key property to make sure this
         // would work even if a user overrides the default Eloquent behaviour
